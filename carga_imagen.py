a118da264a9e68c9f4ef8a761140a674ecc382e8
Gustavo Eduardo <Dr_antraxalergico_55_2@hotmail.com>
2013-06-25 00:23:22 -0500

    cargar una imagen para saber si openCV funciona

---
+++ carga_imagen.py
@@ -0,0 +1,10 @@
+'''Clase del 24 de Junio de 2013 by Gustavo Eduardo'''
+
+im=cv.LoadImage("Documents/Cats_Only.jpg")
+cv.ShowImage('imagen',im)
+cv.WaitKey(0)
+
+'''
+Bibliografía:
+http://www.raerpo.com/2010/12/1-mostrar-una-imagen.html
+'''
